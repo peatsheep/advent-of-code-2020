dd0de2b15092d748ff841a4735a480d71df66859
Peatsheep <github@peatsheep.com>
2020-12-01 00:42:04 -0500

    day 1

---
+++ src/01/index.ts
@@ -0,0 +1,28 @@
+import * as fs from "fs";
+
+// Brute forcing it today bb with a little Nightmare Before Christmas charm
+
+const oogieBoogie = (() => {
+  const data: string = fs.readFileSync("input.txt").toString();
+
+  const lines: string[] = data.split(/\r?\n/);
+
+  const numbers: number[] = lines.map((line) => Number(line));
+
+  let result: number | null = null;
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    const lock = numbers[i];
+    for (let j = i + 1; j < numbers.length - 1; j += 1) {
+      const stock = numbers[j];
+      for (let k = j + 1; k < numbers.length - 2; k += 1) {
+        const barrel = numbers[k];
+        if (lock + stock + barrel === 2020) {
+          return lock * stock * barrel;
+        }
+      }
+    }
+  }
+})();
+
+console.log(oogieBoogie);
